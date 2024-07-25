cb3c9a80443ac8e5a8e65c24830c2cb7a7398a88
artiefellype <arthurfellype21@gmail.com>
2024-07-25 14:37:54 -0300

    feat. about - desk && mobile

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../../templates/home/home.component';
+import { AboutComponent } from "../../templates/about/about.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
