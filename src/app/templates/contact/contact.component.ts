e7dcdab2566a5e12124f9c79550baf6773eb289e
artiefellype <arthurfellype21@gmail.com>
2024-08-16 16:56:54 -0300

    feat. contact

--- src/app/templates/contact/contact.component.ts
+++ src/app/templates/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ResumeButtonComponent } from "../../atoms/resume-button/resume-button.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ResumeButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
