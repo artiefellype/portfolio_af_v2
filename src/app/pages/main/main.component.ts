69d72e44949332be1f996cf03fa91fdf36478fc9
artiefellype <arthurfellype21@gmail.com>
2024-08-16 16:24:55 -0300

    fix. services mobile

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from '../../templates/home/home.component';
 import { AboutComponent } from "../../templates/about/about.component";
 import { ProjectsComponent } from "../../templates/projects/projects.component";
 import { ServicesComponent } from "../../templates/services/services.component";
+import { ContactComponent } from "../../templates/contact/contact.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HomeComponent, AboutComponent, ProjectsComponent, ServicesComponent],
+  imports: [HomeComponent, AboutComponent, ProjectsComponent, ServicesComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
