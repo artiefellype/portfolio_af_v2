9b69b2f9457765d3caab3c0dba3f49a252635d60
artiefellype <arthurfellype21@gmail.com>
2024-08-16 17:08:21 -0300

    feat. footer

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,11 +4,12 @@ import { AboutComponent } from "../../templates/about/about.component";
 import { ProjectsComponent } from "../../templates/projects/projects.component";
 import { ServicesComponent } from "../../templates/services/services.component";
 import { ContactComponent } from "../../templates/contact/contact.component";
+import { FooterComponent } from "../../organisms/footer/footer.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HomeComponent, AboutComponent, ProjectsComponent, ServicesComponent, ContactComponent],
+  imports: [HomeComponent, AboutComponent, ProjectsComponent, ServicesComponent, ContactComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
